8f1712cf1ac414ac5ca187d636e0062f0f24ad84
Ntholi Nkhatho <ntholi@live.com>
2023-07-02 16:22:58 +0200

    handle None marks error

--- workbook_reader.py
+++ workbook_reader.py
@@ -1,12 +1,12 @@
-from model import BorderlineObject
-from openpyxl.styles import PatternFill, Border, Side, Alignment, Font
-from openpyxl.worksheet.worksheet import Worksheet
-from openpyxl.workbook.workbook import Workbook
 from openpyxl.cell.cell import Cell
+from openpyxl.styles import Alignment, Border, Font, PatternFill, Side
 from openpyxl.utils.dataframe import dataframe_to_rows
-from utils.common import convert_list_to_dict, is_number, to_int
+from openpyxl.workbook.workbook import Workbook
+from openpyxl.worksheet.worksheet import Worksheet
 from rich import print
 
+from model import BorderlineObject
+from utils.common import convert_list_to_dict, is_number, to_int
 
 PARSER = "html5lib"
 
@@ -16,10 +16,10 @@ def get_marks_cols(sheet: Worksheet):
     for col in sheet.iter_cols():
         for c in col:
             cell: Cell = c
-            if cell.value == 'Mk':
+            if cell.value == "Mk":
                 col_i = cell.col_idx
                 row_i = cell.row
-                grade_cell: Cell = sheet.cell(row_i-2, col_i)
+                grade_cell: Cell = sheet.cell(row_i - 2, col_i)
                 # courses[grade_cell.value] = grade_cell.column
                 courses[grade_cell.column] = grade_cell.value
     return courses
@@ -29,7 +29,7 @@ def get_std_column(sheet: Worksheet):
     for col in sheet.iter_cols():
         for c in col:
             cell: Cell = c
-            if cell.value == 'StudentID':
+            if cell.value == "StudentID":
                 return cell.column
     return 3
 
@@ -38,7 +38,7 @@ def get_remark_col(sheet: Worksheet):
     for col in sheet.iter_cols():
         for c in col:
             cell: Cell = c
-            if cell and cell.value and 'remark' in str(cell.value).lower():
+            if cell and cell.value and "remark" in str(cell.value).lower():
                 return cell.column
     return -1
 
@@ -74,9 +74,7 @@ def read_excel_marks(sheet: Worksheet):
             mark_value = None
             if is_number(cell.value):
                 mark_value = float(cell.value)
-                data.append({
-                    course_code: mark_value
-                })
+                data.append({course_code: mark_value})
         results[student_number] = data
 
     return convert_list_to_dict(results)
@@ -93,14 +91,14 @@ def get_border_line_objects(sheet: Worksheet):
             cell: Cell = sheet.cell(student_col, mark_col)
             mark_value = None
             if is_number(cell.value):
-                grade: Cell = sheet.cell(student_col, mark_col+1)
+                grade: Cell = sheet.cell(student_col, mark_col + 1)
                 mark_value = float(cell.value)
                 borderline = BorderlineObject(
                     std_no=student_number,
                     std_class=sheet.title,
                     course=course_code,
                     marks=mark_value,
-                    grade=grade.value
+                    grade=grade.value,
                 )
                 data.append(borderline)
 
@@ -113,7 +111,15 @@ def combine_cms_excel_marks(excel_marks: dict[int, dict], cms_marks: dict[int, d
     for std, results in cms_marks.items():
         items = {}
         for course, credits in results.items():
-            items[course] = credit_hours_to_marks(credits)
+            marks = 0
+            if credits:
+                marks = credit_hours_to_marks(credits)
+            # print("std", std)
+            # print("course", course)
+            # print("credits", credits)
+            # print("marks", marks)
+
+            items[course] = marks
         data[std] = items
 
     for std, results in excel_marks.items():
@@ -126,7 +132,6 @@ def combine_cms_excel_marks(excel_marks: dict[int, dict], cms_marks: dict[int, d
 
 
 def generate_remarks(sheet: Worksheet, cms_marks: dict[int, dict]):
-
     excel_marks = read_excel_marks(sheet)
     students = get_student_numbers(sheet)
     marks_dict = combine_cms_excel_marks(excel_marks, cms_marks)
@@ -165,5 +170,5 @@ def main():
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(main())
